0aa6e75d4cd4211cfa913e14b8ba44ed771687f0
Greg Mall <gregmall157@gmail.com>
2020-08-18 14:47:38 -0700

    edit

--- data/todos.js
+++ data/todos.js
@@ -1,16 +1,16 @@
 module.exports = [
   {
-    user_id: 1,
+    
     todo: 'feed the cats',
     completed: false,
   },
   {
-    user_id: 2,
+    
     todo: 'water the plants',
     completed: false,
   },
   {
-    user_id: 1,
+    
     todo: 'do the dishes',
     completed: false,
   }
