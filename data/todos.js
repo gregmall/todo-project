460f991b649e609fb67e73ab4a5f3650b84a337a
Greg Mall <gregmall157@gmail.com>
2020-08-18 14:14:22 -0700

    first endpoint

---
+++ data/todos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    user_id: 1,
+    todo: 'feed the cats',
+    completed: false,
+  },
+  {
+    user_id: 2,
+    todo: 'water the plants',
+    completed: false,
+  },
+  {
+    user_id: 1,
+    todo: 'do the dishes',
+    completed: false,
+  }
+];
